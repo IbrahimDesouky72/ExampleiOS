e663cafb6b2fa5099f43e5ed3cd5ba4599574c4f
Ibrahim Desouky Ibrahim <ibrahim.desouky44@gmail.com>
2024-01-07 21:23:21 +0200

    add button and text field with action

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var greetingLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func helloButtonClicked(_ sender: Any) {
+        greetingLabel.text = "Hello \(textField.text ?? "")!"
+    }
 }
 
